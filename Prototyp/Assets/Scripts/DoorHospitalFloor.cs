51afc37bcec97fc250e6fac629a4d3a3f23db439
Vitus <vitus.maierhoefer@gmail.com>
2018-02-22 20:28:41 +0100

    Added new Doors and Scripts Part 1

---
+++ Prototyp/Assets/Scripts/DoorHospitalFloor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHospitalFloor : Interactable
+{
+
+	public AudioClip OpenDoorSound;
+	public AudioClip CloseDoorSound;
+	public AudioClip DoorLockedSound;
+	public AudioClip DontGoBackWarning;
+
+	public AudioClip Dialogue_S_1_0;
+
+	Animator animator;
+	bool doorOpen;
+	bool doorAllowedToOpen;
+
+	void Start ()
+	{
+		print ("In Start()");
+		doorOpen = false;
+		doorAllowedToOpen = true;
+		animator = GetComponent<Animator> ();
+	}
+
+
+	public override void OnInteraction ()
+	{
+		print ("On Interaction with the Door from Hospital to Floor!");
+
+		if (doorAllowedToOpen) {
+
+			doorOpen = true;
+			doorAllowedToOpen = false;
+			DoorControl ("Open");
+
+			SoundManager.instance.PlayEffect (OpenDoorSound, 0.5f);
+		} else {
+			print ("Not allowed to open door!");
+			if (!doorOpen) {
+				SoundManager.instance.PlayEffect (DoorLockedSound);
+			}
+			if (!SoundManager.instance.GetDialoguePlaying ()) {
+				SoundManager.instance.PlayTestManagerDialogue (DontGoBackWarning, 0.5f);
+			}
+		}
+	}
+
+
+	void DoorControl (string direction)
+	{
+		animator.SetTrigger (direction);
+	}
+
+
+	void OnTriggerExit (Collider col)
+	{
+
+		if (doorOpen) {
+
+			doorOpen = false;
+			DoorControl ("Close");
+
+			SoundManager.instance.PlayEffect (CloseDoorSound, 0.5f);
+			SoundManager.instance.PlayCombinedDialogue (Dialogue_S_1_0, 1f);
+		}
+	}
+
+	//temp
+	public void OpenDoor ()
+	{
+		DoorControl ("Open");
+	}
+
+}
---
+++ Prototyp/Assets/Scripts/RubberDuck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RubberDuck : Interactable
+{
+
+	public AudioClip Quack01;
+	public AudioClip Quack02;
+	public AudioClip Quack03;
+	public AudioClip Quack04;
+
+	AudioClip[] quackArray;
+
+	void Start ()
+	{
+		quackArray = new AudioClip[] { Quack01, Quack02, Quack03, Quack04 };
+	}
+
+
+	public override void OnInteraction ()
+	{
+		print ("On Interaction with Duck!");
+
+		AudioClip randomQuack = quackArray[Random.Range(0, quackArray.Length)];
+
+		SoundManager.instance.PlayEffect (randomQuack);
+
+	}
+}
