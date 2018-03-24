21f8eadeec3812ffcdc1bb4a4f003395a644bfac
esolneman <marlena.wolfes@gmx.de>
2018-03-24 20:20:09 +0100

    update rabbit script

--- TeamMergeConflict_Subject42/Assets/Scripts/AnimatedTexture_Rabbit_Sad.js
+++ TeamMergeConflict_Subject42/Assets/Scripts/AnimatedTexture_Rabbit_Sad.js
@@ -28,6 +28,6 @@ function Update () {
 	// build offset
 	// v coordinate is the bottom of the image in opengl so we need to invert.
 	//var offset = Vector2 (uIndex * size.x, 1.0 - size.y - vIndex * size.y);
-	var offset = Vector2 (uIndex * size.x, 1.0);
+	var offset = Vector2 (uIndex * size.x, 0.0);
 	GetComponent.<Renderer>().material.SetTextureOffset ("_MainTex", offset);
 }
