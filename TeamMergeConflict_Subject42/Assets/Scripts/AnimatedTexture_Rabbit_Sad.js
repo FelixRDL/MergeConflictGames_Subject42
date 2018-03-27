c36871f6baf90aeb92dc1abac5d83612c2718d52
esolneman <marlena.wolfes@gmx.de>
2018-03-28 00:25:24 +0200

    update sad_rabbit script

--- TeamMergeConflict_Subject42/Assets/Scripts/AnimatedTexture_Rabbit_Sad.js
+++ TeamMergeConflict_Subject42/Assets/Scripts/AnimatedTexture_Rabbit_Sad.js
@@ -1,33 +1,54 @@
 //	Code used from http://wiki.unity3d.com/index.php?title=Animating_Tiled_texture", Courtesy to Joachim Ante (16.2.18)
-
+// and from https://unity3d.com/de/learn/tutorials/topics/scripting/coroutines
 
 var uvAnimationTileX = 2; //Here you can place the number of columns of your sheet.
                            //The above sheet has 24
 
 var uvAnimationTileY = 1; //Here you can place the number of rows of your sheet.
                           //The above sheet has 1
-var framesPerSecond = 1.0;
+var framesPerSecondEyeOpen : float = 4.0f;
+var framesPerSecondEyeClosed : float = 0.2f;
+var framesPerSecond : float = 5.0f;
+var isOpen = true;
 
-function Update () {
-
-	// Calculate index
-	var index : int = Time.time * framesPerSecond;
-	// repeat when exhausting all frames
-	index = index % (uvAnimationTileX * uvAnimationTileY);
-
-	// Size of every tile
-	var size = Vector2 (1.0 / uvAnimationTileX, 1.0 / uvAnimationTileY);
-	//var size = Vector2 (1024, 1024);
-
-	// split into horizontal and vertical index
-	var uIndex = index % uvAnimationTileX;
-	// var vIndex = index / uvAnimationTileX;
-
-	//var vIndex = index % uvAnimationTileX;
-
-	// build offset
-	// v coordinate is the bottom of the image in opengl so we need to invert.
-	//var offset = Vector2 (uIndex * size.x, 1.0 - size.y - vIndex * size.y);
-	var offset = Vector2 (uIndex * size.x, 0.0);
-	GetComponent.<Renderer>().material.SetTextureOffset ("_MainTex", offset);
+function Start () 
+{
+	framesPerSecond = framesPerSecondEyeOpen;
+	isOpen = true;
+    MyCoroutine();
 }
+
+function MyCoroutine ()
+{
+    while(true)
+    {
+        if(isOpen == true){
+			framesPerSecond = framesPerSecondEyeOpen;
+			isOpen = false;
+		}else{
+			framesPerSecond = framesPerSecondEyeClosed;
+			isOpen = true;
+		}
+		print(isOpen);
+		print("FPS");
+		print(framesPerSecond);
+		// Calculate index
+		var index : int = Time.time * framesPerSecond;
+		// repeat when exhausting all frames
+		index = index % (uvAnimationTileX * uvAnimationTileY);
+		// Size of every tile
+		var size = Vector2 (1.0 / uvAnimationTileX, 1.0 / uvAnimationTileY);
+		// split into horizontal and vertical index
+		var uIndex = index % uvAnimationTileX;
+		// build offset
+		// v coordinate is the bottom of the image in opengl so we need to invert.
+		var offset = Vector2 (uIndex * size.x, 0.0);
+		GetComponent.<Renderer>().material.SetTextureOffset ("_MainTex", offset);
+        yield WaitForSeconds(framesPerSecond);
+    }
+    
+    print("Reached the target.");
+    print("MyCoroutine is now finished.");
+}
+
+
