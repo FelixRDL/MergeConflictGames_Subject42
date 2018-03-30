67666e808fc8f2229182e150ffa3aa9a240f2637
Vitus <vitus.maierhoefer@gmail.com>
2018-03-30 16:05:24 +0200

    Commenting part 1

--- TeamMergeConflict_Subject42/Assets/Scripts/EmergencyLights.cs
+++ TeamMergeConflict_Subject42/Assets/Scripts/EmergencyLights.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//If this Script is active, it controls the rotation of the Emergency Light of the ambulance
 public class EmergencyLights : MonoBehaviour
 {
+	//The speed of the rotating light
+	public float rotationSpeed = 300f;
 
-	void Update ()
+	void Start ()
 	{
-		transform.Rotate (new Vector3 (0f, Time.deltaTime * 400f, 0f));	
+		StartCoroutine (RotateLightCoroutine ());
 	}
+
+	//The loop is in a coroutine for performance reasons.
+	IEnumerator RotateLightCoroutine ()
+	{
+		while (true) {
+			transform.Rotate (new Vector3 (0f, Time.deltaTime * rotationSpeed, 0f));	
+			yield return null;
+		}
+	}
+
 }
